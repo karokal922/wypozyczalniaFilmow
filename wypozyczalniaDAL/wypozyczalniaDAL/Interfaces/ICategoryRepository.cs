68c6c778e71d2e68c3c8ee2926b2ace0ae4e9403
PawelSzot <paweszo428@student.polsl.pl>
2023-03-29 20:37:40 +0200

    :+1:

---
+++ wypozyczalniaDAL/wypozyczalniaDAL/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wypozyczalniaDAL.Models;
+
+namespace wypozyczalniaDAL.Interfaces
+{
+    public interface ICategoryRepository : IDisposable
+    {
+        IEnumerable<Category> GetCategories();
+        Category GetCategory(int id);
+        void InsertCategory(Category category);
+        void DeleteCategory(int id);
+        void UpdateCategory(Category category);
+        void Save();
+        
+    }
+}
---
+++ wypozyczalniaDAL/wypozyczalniaDAL/Interfaces/IMovieRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wypozyczalniaDAL.Models;
+
+namespace wypozyczalniaDAL.Interfaces
+{
+    public interface IMovieRepository : IDisposable
+    {
+        IEnumerable<Movie> GetMovies();
+        Movie GetMovie(int id);
+        void InsertMovie(Movie movie);
+        void DeleteMovie(int id);
+        void UpdateMovie(Movie movie);
+        void Save();
+
+    }
+}
