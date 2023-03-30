302eb3e3a56cc860bb9a0380ee5e8dc19f23cbd6
karokal922 <73292943+karokal922@users.noreply.github.com>
2023-03-30 13:09:11 +0200

    Repositories and Context

---
+++ wypozyczalniaDAL/wypozyczalniaDAL/Models/MovieRentalContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wypozyczalniaDAL.Models
+{
+    public class MovieRentalContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Rent> Rentals { get; set; }
+        public DbSet<Rate> Ratings { get; set; }
+        public void OnModelCreating(DbContextOptionsBuilder modelBuilder)
+        {
+            modelBuilder.UseSqlServer();
+        }
+
+    }
+}
