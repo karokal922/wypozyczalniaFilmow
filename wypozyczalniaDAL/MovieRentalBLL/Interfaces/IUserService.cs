605d33e7f71d3dde0b1f9cee1a0fcb9a489dd730
karokal922 <73292943+karokal922@users.noreply.github.com>
2023-04-19 20:07:25 +0200

    IUserService

---
+++ wypozyczalniaDAL/MovieRentalBLL/Interfaces/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wypozyczalniaDAL.Models;
+
+namespace MovieRentalBLL.Interfaces
+{
+    public interface IUserService
+    {
+        public IEnumerable<User> GetUsersWithRateCountSorted();
+        public IEnumerable<User> GetUsersByNameSorted(string name);
+    }
+}
