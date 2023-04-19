b660385484573a07566325d55a989c7b56dd9708
pawelkolodziej143 <pawekol143@student.polsl.pl>
2023-04-19 15:40:51 +0200

    repair

--- wypozyczalniaDAL/wypozyczalniaDAL/Interfaces/IPaymentRepository.cs
+++ wypozyczalniaDAL/wypozyczalniaDAL/Interfaces/IPaymentRepository.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using wypozyczalniaDAL.Models;
 
 namespace wypozyczalniaDAL.Interfaces
 {
-    internal class IPaymentRepository
+    public interface IPaymentRepository : IDisposable
     {
+        IEnumerable<Payment> GetPayments();
+        Payment GetPayment(int id);
+        void InsertPayment(Payment payment);
+        void DeletePayment(int id);
+        void UpdatePayment(Payment payment);
+        void Save();
     }
 }
