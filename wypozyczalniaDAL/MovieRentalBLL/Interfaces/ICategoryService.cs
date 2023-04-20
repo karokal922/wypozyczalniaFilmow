8d2d6e230e07c53170bc250472d58aac9c65a397
karokal922 <73292943+karokal922@users.noreply.github.com>
2023-04-20 21:37:10 +0200

    Error repair

---
+++ wypozyczalniaDAL/MovieRentalBLL/Interfaces/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRentalBLL.Interfaces
+{
+    public interface ICategoryService
+    {
+        Dictionary<string, List<string>> GetMovieTitlesByCategories(params string[] categories);
+        IEnumerable<object> GetCategoriesWithMovieCount();
+    }
+}
