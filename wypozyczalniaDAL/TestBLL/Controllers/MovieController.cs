543fca69a6e1375b2fd9b7c215b3c12b0d9d225a
karokal922 <73292943+karokal922@users.noreply.github.com>
2023-04-20 18:41:35 +0200

    TestBLL project

---
+++ wypozyczalniaDAL/TestBLL/Controllers/MovieController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestBLL.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ wypozyczalniaDAL/TestBLL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestBLL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
