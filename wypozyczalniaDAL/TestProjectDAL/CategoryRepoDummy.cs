9c66d78ce77e1ebfa58123b6e256ad3a1ae08145
PawelSzot <paweszo428@student.polsl.pl>
2023-05-09 19:37:38 +0200

    Dummies

---
+++ wypozyczalniaDAL/TestProjectDAL/CategoryRepoDummy.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wypozyczalniaDAL.Interfaces;
+using wypozyczalniaDAL.Models;
+
+namespace TestProjectDAL
+{
+    public class CategoryRepoDummy : ICategoryRepository
+    {
+        public void DeleteCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ wypozyczalniaDAL/TestProjectDAL/MovieRepoDummy.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wypozyczalniaDAL.Interfaces;
+using wypozyczalniaDAL.Models;
+
+namespace TestProjectDAL
+{
+    public class MovieRepoDummy : IMovieRepository
+    {
+        public void DeleteMovie(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Movie GetMovie(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Movie> GetMovies()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
