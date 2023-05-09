626ad87d0dc39aa083d3012c5d13547464a9c58a
PawelSzot <paweszo428@student.polsl.pl>
2023-05-09 19:41:32 +0200

    Dummies

---
+++ wypozyczalniaDAL/TestProjectDAL/RentRepoDummy.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wypozyczalniaDAL.Interfaces;
+using wypozyczalniaDAL.Models;
+
+namespace TestProjectDAL
+{
+    public class RentRepoDummy : IRentRepository
+    {
+        public void DeleteRent(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Rent GetRent(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Rent> GetRents()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertRent(Rent rent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateRent(Rent rent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ wypozyczalniaDAL/TestProjectDAL/UserRepoDummy.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wypozyczalniaDAL.Interfaces;
+using wypozyczalniaDAL.Models;
+
+namespace TestProjectDAL
+{
+    public class UserRepoDummy : IUserRepository
+    {
+        public void DeleteUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
