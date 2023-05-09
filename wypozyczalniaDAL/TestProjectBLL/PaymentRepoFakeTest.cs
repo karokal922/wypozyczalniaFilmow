1e70ee996273f941d9cca390ded27ffed681d238
PawelSzot <paweszo428@student.polsl.pl>
2023-05-09 17:44:35 +0200

    :+1:

---
+++ wypozyczalniaDAL/TestProjectBLL/PaymentRepoFakeTest.cs
@@ -0,0 +1,24 @@
+﻿using MovieRentalBLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestDAL;
+using wypozyczalniaDAL.Models;
+using wypozyczalniaDAL.Repositories;
+
+namespace TestProjectBLL
+{
+    public class PaymentRepoFakeTest
+    {
+        [Fact]
+        public void GetMoviesByCategoryTest()
+        {
+            var movieRepo = new MovieRepoFake();
+            var unityOfWork = new UnitOfWork(movieRepo);
+            var movieBLL = new MovieService(unityOfWork);
+
+        }
+    }
+}
