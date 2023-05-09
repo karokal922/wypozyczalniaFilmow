ac3a3293435726df744210e0f4371bc89fbf4293
PawelSzot <paweszo428@student.polsl.pl>
2023-05-09 19:39:46 +0200

    Dummies

---
+++ wypozyczalniaDAL/TestProjectDAL/PaymentRepoDummy.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wypozyczalniaDAL.Interfaces;
+using wypozyczalniaDAL.Models;
+
+namespace TestProjectDAL
+{
+    public class PaymentRepoDummy : IPaymentRepository
+    {
+        public void DeletePayment(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Payment GetPayment(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Payment> GetPayments()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertPayment(Payment payment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePayment(Payment payment)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ wypozyczalniaDAL/TestProjectDAL/RateRepoDummy.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wypozyczalniaDAL.Interfaces;
+using wypozyczalniaDAL.Models;
+
+namespace TestProjectDAL
+{
+    public class RateRepoDummy : IRateRepository
+    {
+        public void DeleteRate(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Rate GetRate(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Rate> GetRates()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertRate(Rate rate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateRate(Rate rate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
