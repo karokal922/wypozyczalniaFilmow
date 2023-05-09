b58d40bc6260520ccc8f46253e2a96cc68ced899
karokal922 <73292943+karokal922@users.noreply.github.com>
2023-05-09 19:55:05 +0200

    UnitTestUnitOfWork

---
+++ wypozyczalniaDAL/TestProjectDAL/UnitTestUnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wypozyczalniaDAL.Interfaces;
+using wypozyczalniaDAL.Repositories;
+
+namespace TestProjectDAL
+{
+    public class UnitTestUnitOfWork
+    {
+        [Fact]
+        public void TestUnitOfWork()
+        {
+            var categoryRepository=new CategoryRepoDummy();
+            var movieRepository=new MovieRepoDummy();
+            var paymentRepository=new PaymentRepoDummy();
+            var rateRepository=new RateRepoDummy();
+            var rentRepository=new RentRepoDummy();
+            var userRepository=new UserRepoDummy();
+
+            var unitOfWork = new UnitOfWork(null,categoryRepository,movieRepository,paymentRepository,rateRepository,rentRepository,userRepository);
+
+            Assert.Same(categoryRepository, unitOfWork.CategoryRepository);
+            Assert.Same(movieRepository, unitOfWork.MovieRepository);
+            Assert.Same(paymentRepository, unitOfWork.PaymentRepository);
+            Assert.Same(rateRepository, unitOfWork.RateRepository);
+            Assert.Same(rentRepository, unitOfWork.RentRepository);
+            Assert.Same(userRepository, unitOfWork.UserRepository);
+        }
+    }
+}
