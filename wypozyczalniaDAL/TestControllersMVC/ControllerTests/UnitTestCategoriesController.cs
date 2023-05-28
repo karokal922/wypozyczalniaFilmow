e85ad3bed3c19b10031931a8b85d7825eb2e82cf
karokal922 <73292943+karokal922@users.noreply.github.com>
2023-05-28 11:20:01 +0200

    adding UnitTestControllers files

---
+++ wypozyczalniaDAL/TestControllersMVC/ControllerTests/UnitTestCategoriesController.cs
@@ -0,0 +1,16 @@
+using MovieRentalBLL;
+using wypozyczalniaDAL;
+using Moq;
+
+
+namespace TestControllersMVC.ControllerTests
+{
+    public class UnitTestCategoriesController
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
